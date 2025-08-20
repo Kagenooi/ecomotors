e2ecd0a4aa0895e5b4f9b9dbfd0d924cbd1e5bec
Kagenooi <kagenooi.morpheus@gmail.com>
2025-08-21 02:06:50 +0400

    homepage, without footer

--- js/index.js
+++ js/index.js
@@ -6,6 +6,14 @@ window.toggleActive = toggleActive;
 
 
 const productSwiper = new Swiper(".productSwiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+const headerSwiper = new Swiper(".headerSwiper", {
+    loop: true,
+    autoplay: true,
     pagination: {
         el: ".swiper-pagination",
         dynamicBullets: true,
