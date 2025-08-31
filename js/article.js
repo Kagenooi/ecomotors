cf958a461d8eb5840e8d78b7813a172526e417ba
Kagenooi <kagenooi.morpheus@gmail.com>
2025-09-01 02:03:35 +0400

    single article

---
+++ js/article.js
@@ -0,0 +1,10 @@
+import {
+    toggleActive,
+    closeMenu,
+    closeMenuBtn
+} from "./module.js";
+
+window.toggleActive = toggleActive;
+window.closeMenu = closeMenu;
+closeMenu();
+window.closeMenuBtn = closeMenuBtn;
