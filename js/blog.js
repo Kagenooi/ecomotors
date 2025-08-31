715711abe3b667f0c8b4144479e9b4f19f18dac4
Kagenooi <kagenooi.morpheus@gmail.com>
2025-08-31 22:25:25 +0400

    blog

--- js/blog.js
+++ js/blog.js
@@ -4,7 +4,21 @@ import {
     closeMenuBtn
 } from "./module.js";
 
-window.toggleActive = toggleActive; 
+window.toggleActive = toggleActive;
 window.closeMenu = closeMenu;
 closeMenu();
 window.closeMenuBtn = closeMenuBtn;
+
+
+
+const blogSlider = new Swiper("#blogSlider", {
+    navigation: {
+        nextEl: ".header__slider_btn.next",
+        prevEl: ".header__slider_btn.prev",
+    },
+    autoplay: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
