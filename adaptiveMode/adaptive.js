34d95778ff299249095688046517e1b0b90e0021
Kagenooi <kagenooi.morpheus@gmail.com>
2025-09-14 01:52:06 +0400

    last push?

--- adaptiveMode/adaptive.js
+++ adaptiveMode/adaptive.js
@@ -12,13 +12,12 @@ export function adaptive() {
     }
     wrapper.style.zoom = zoom;
 
+    // сохраняем значение zoom для других частей кода (удобно)
+    wrapper.dataset.zoom = String(zoom);
     if (document.body.clientWidth < 769) {
-        // сохраняем значение zoom для других частей кода (удобно)
-        wrapper.dataset.zoom = String(zoom);
-
         // --- корректируем модалки ---
         // желаемая видимая высота (в пикселях) — можно брать долю окна
-        const desiredVisiblePx = Math.round(window.innerHeight * 1); // 90% vh
+        const desiredVisiblePx = Math.round(window.innerHeight * 1); // 100% vh
         document.querySelectorAll('.burger').forEach(modal => {
             // высота, которую нужно присвоить до масштабирования
             const neededHeight = Math.max(Math.round(desiredVisiblePx / zoom), 240); // минимум 240px
@@ -26,6 +25,23 @@ export function adaptive() {
             // удобно задать и max-height, чтобы скролл внутри работал корректно
             modal.style.maxHeight = Math.round(window.innerHeight / zoom) + 'px';
             modal.style.overflowY = 'auto';
+        });
+    }
+
+    if (document.body.clientWidth > 1281) {
+        const topAds = Math.round(window.innerHeight * 1); // 90% vh
+        document.querySelectorAll('.topAds__item').forEach(modal => {
+            // высота, которую нужно присвоить до масштабирования
+            const neededHeight = Math.max(Math.round(topAds / zoom), 0); // минимум 240px
+
+            // удобно задать и max-height, чтобы скролл внутри работал корректно
+            modal.style.zoom = neededHeight / 1000;
+
+            let moreSize = modal.clientWidth * (neededHeight / 1000) * 2;
+            let trueSize = 1630 - (234 * 2) + moreSize;
+            modal.parentElement.style.maxWidth = trueSize + 'px';
+            modal.parentElement.style.minWidth = trueSize + 'px';
+
         });
     }
 }
