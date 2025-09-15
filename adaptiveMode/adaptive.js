cb1d8636b3d9f92d190edb5fab71908d0ed66601
Kagenooi <kagenooi.morpheus@gmail.com>
2025-09-15 17:58:01 +0400

    dfgh

--- adaptiveMode/adaptive.js
+++ adaptiveMode/adaptive.js
@@ -1,9 +1,6 @@
 export function adaptive() {
     const wrapper = document.querySelector('#adaptive');
     let zoom = document.body.clientWidth / 19.2 / 100;
-    if (document.body.clientWidth > 1281 && document.body.clientWidth < 1630) {
-        zoom = document.body.clientWidth / 16.3 / 100;
-    }
     if (document.body.clientWidth > 768 && document.body.clientWidth < 1281) {
         zoom = document.body.clientWidth / 11.5 / 100;
     }
